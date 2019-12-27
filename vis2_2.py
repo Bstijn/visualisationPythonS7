adabf5bf7669d7172f9a33318563383304654ee4
Bstijn <baltessen.stijn@gmail.com>
2019-12-27 13:43:35 +0100

    assignment 2.2 and 2.4

--- vis2_2.py
+++ vis2_2.py
@@ -1,5 +1,7 @@
 from vtkmodules.all import (
-    vtkPolyDataReader, vtkPolyDataMapper, vtkActor
+    vtkPolyDataReader, vtkProperty,
+    vtkPolyDataMapper, vtkActor,
+    vtkStructuredPointsReader, vtkGeometryFilter,
 )
 from util.window_renderer import WindowRenderer
 from vis1_3 import Cylinder
@@ -8,24 +10,43 @@ from vis1 import Cone
 from vis2_1 import ObjVisualizer
 
 class VtkVisualizer:
-    def __init__(self, windowrenderer):
-        self.__renderer = windowrenderer
-        self.__vtk_reader = vtkPolyDataReader()
-        self.__actor = vtkActor()
-        self.__data_mapper = vtkPolyDataMapper()
+    def __init__(self, renderer):
+        # Renderer variable is needed to add the actor
+        self.__renderer = renderer
 
-    def setup_object(self,filepath):
-        #get file
-        self.__vtk_reader.SetFileName(filepath)
-        # setup mapper and actor
-        self.__data_mapper.SetInputConnection(self.__vtk_reader.GetOutputPort())
-        self.__actor.SetMapper(self.__data_mapper)
-        # renderer
+        self.__reader = vtkStructuredPointsReader()
+        self.__property = vtkProperty()
+        self.__geometry_filter = vtkGeometryFilter()
+        self.__mapper = vtkPolyDataMapper()
+        self.__actor = vtkActor()
+
+    def setup(self, file_name):
+        """Setup the VTK structured points reader"""
+
+        # Set reader
+        self.__reader.SetFileName(file_name)
+        self.__reader.Update()
+
+        # Set property
+        self.__property.SetColor(1.0, 0.0, 0.0)
+
+        self.__geometry_filter.SetInputConnection(self.__reader.GetOutputPort())
+        self.__geometry_filter.Update()
+
+        # Set mapper
+        self.__mapper.SetInputConnection(self.__geometry_filter.GetOutputPort())
+        self.__mapper.Update()
+
+        # Set  actor
+        self.__actor.SetProperty(self.__property)
+        self.__actor.SetMapper(self.__mapper)
+
+        # Add actor to the window renderer
         self.__renderer.AddActor(self.__actor)
 
 if __name__ == '__main__':
     window_renderer = WindowRenderer()
-    VtkVisualizer(window_renderer.renderer).setup_object("vtkfiles/brain.vtk")
+    VtkVisualizer(window_renderer.renderer).setup("vtkfiles/brain.vtk")
     #setup and start window
     window_renderer.setup_render_window()
     window_renderer.start_render_window()
---
+++ vis2_4.py
@@ -0,0 +1,44 @@
+from vtkmodules.all import (
+    vtkPlaneSource, vtkBMPReader,
+    vtkTexture, vtkPolyDataMapper,
+    vtkActor,
+)
+
+from util.window_renderer import WindowRenderer
+
+class VtkTextureReader:
+    def __init__(self, windowrenderer):
+        #Renderer
+        self.__renderer = windowrenderer
+
+        #vtk import variables
+        self.__plane = vtkPlaneSource()
+        self.__reader = vtkBMPReader()
+        self.__texture = vtkTexture()
+        self.__mapper = vtkPolyDataMapper()
+        self.__actor = vtkActor()
+
+    def setup(self,file_name):
+        # Reader
+        self.__reader.SetFileName(file_name)
+        self.__reader.Update()
+        #Texture
+        self.__texture.SetInputConnection(self.__reader.GetOutputPort())
+
+        # Mapper
+        self.__mapper.SetInputConnection(self.__plane.GetOutputPort())
+
+        # Actor
+        self.__actor.SetTexture(self.__texture)
+        self.__actor.SetMapper(self.__mapper)
+
+        # Renderer
+        self.__renderer.AddActor(self.__actor)
+
+
+if __name__ == '__main__':
+    windowrenderer = WindowRenderer()
+    VtkTextureReader(windowrenderer.renderer).setup("images/marbles.bmp")
+
+    windowrenderer.setup_render_window()
+    windowrenderer.start_render_window()
